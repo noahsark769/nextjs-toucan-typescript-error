a33955b31736d1551255c8ae586aefa53a339bab
Noah Gilmore <noah.w.gilmore@gmail.com>
2024-02-10 22:20:03 -0800

    Even simpler

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import { useEffect } from "react";
-import { Toucan } from "toucan-js";
 
 export default function Page() {
-  const sentry = new Toucan({
-    dsn: "",
-    request: undefined,
-  });
-  sentry.captureException(new Error("Hello"));
   useEffect(() => {
     const script = document.createElement("script");
     document.body.append(script);
